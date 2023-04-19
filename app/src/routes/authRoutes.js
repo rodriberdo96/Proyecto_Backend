26782a79d5f9b619fffa827557638ab3af56f847
Rodri Berdomas <rodriberdomas@gmail.com>
2023-04-19 11:29:11 -0300

    correcion

--- app/src/routes/authRoutes.js
+++ app/src/routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express =  require('express')
 const routesAuth = express.Router()
 
-const authControllers = require('/controllers/authController')
+const authControllers = require('../controllers/authController')
 const authController = new authControllers()
 
 
