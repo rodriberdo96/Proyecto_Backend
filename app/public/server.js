bc0f272207574039bd65a57f25d2fcfad9f500e2
Rodri Berdomas <rodriberdomas@gmail.com>
2023-04-19 23:29:10 -0300

    correcion

--- app/public/server.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config()
-const logger = require('../src/utils/winston')
-
-const { httpServer } = require('../src/httpServer')
-
-const PORT = process.env.PORT || 8080
-
-httpServer.listen(PORT, () => logger.info(`http://localhost:${PORT}/`))
