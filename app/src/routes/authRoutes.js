02b69eeb6cf7eb70b3e2f576f434949cb8883800
Rodri Berdomas <rodriberdomas@gmail.com>
2023-04-24 17:35:46 -0300

    agregado ruta get en auth

--- app/src/routes/authRoutes.js
+++ app/src/routes/authRoutes.js
@@ -4,9 +4,11 @@ const routesAuth = express.Router()
 const authControllers = require('../controllers/authController')
 const authController = new authControllers()
 
-
+routesAuth.get ('/register', authController.register)
 routesAuth.post('/register', authController.register)
+routesAuth.get ('/login', authController.login)
 routesAuth.post('/login', authController.login)
+
 
 
 module.exports = { routesAuth }
